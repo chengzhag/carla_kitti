7a862845286da98c335d7031b5d6874d2f1e37c1
Unknown <850576570@qq.com>
2019-02-08 13:30:21 +0800

    Fixed repeat start point problem.

--- generate.py
+++ generate.py
@@ -17,9 +17,7 @@ from carla.util import print_over_same_line
 
 
 def run_carla_client(args):
-    number_of_episodes = 40
-    frames_per_episode = 100
-
+    # camera settings
     camcoor_x = 0.27
     camcoor_y = -0.06
     camcoor_z = 1.65
@@ -27,62 +25,76 @@ def run_carla_client(args):
     resolution_w = 1240 * args.scale
     resolution_h = 376 * args.scale
     camfu = 718.856 * args.scale
-    camFOV = 2 * math.atan2( resolution_w , 2 * camfu ) * 180 / math.pi
+    camFOV = 2 * math.atan2(resolution_w, 2 * camfu) * 180 / math.pi
 
     # skip n frames for vehicle to start
     num_frames_skip = 30
 
+    # weather settings
+    weathers = [1, 2, 8, 9]
+    print('weathers = ')
+    print(weathers)
+
     with make_carla_client(args.host, args.port) as client:
         print('CarlaClient connected')
 
-        for episode in range(0, number_of_episodes):
+        # scene = client.load_settings(new_setting())
+
+        # Choose one player start at random.
+        number_of_player_starts = 152
+        if args.num_starts != 0:
+            number_of_episodes = min(number_of_player_starts, args.num_starts)
+
+        print('%d episodes will be generated...' % number_of_episodes)
+
+        startPoints = list(range(0, number_of_player_starts))
+        startPoints = random.sample(startPoints, number_of_episodes)
+
+        # Create a CarlaSettings object. This object is a wrapper around
+        # the CarlaSettings.ini file. Here we set the configuration we
+        # want for the new episode.
+        settings = CarlaSettings()
+        settings.set(
+            SynchronousMode=True,
+            SendNonPlayerAgentsInfo=True,
+            NumberOfVehicles=40,
+            NumberOfPedestrians=0,
+            WeatherId=random.choice(weathers),
+            QualityLevel=args.quality_level)
+        settings.randomize_seeds()
+
+        # Now we want to add a couple of cameras to the player vehicle.
+        # We will collect the images produced by these cameras every
+        # frame.
+
+        for cameraID in range(2, 4):
+            camcoor_y2 = camcoor_y
+            # The default camera captures RGB images of the scene.
+            cameraRGB = Camera('Camera%dRGB' % cameraID, FOV=camFOV)
+            # Set image resolution in pixels.
+            cameraRGB.set_image_size(resolution_w, resolution_h)
+            # Set its position relative to the car in meters.
+            cameraRGB.set_position(camcoor_x, camcoor_y2 if cameraID == 2 else camcoor_y3, camcoor_z)
+            settings.add_sensor(cameraRGB)
+
+            # Let's add another camera producing ground-truth depth.
+            cameraDepth = Camera('Camera%dDepth' % cameraID, PostProcessing='Depth', FOV=camFOV)
+            cameraDepth.set_image_size(resolution_w, resolution_h)
+            cameraDepth.set_position(camcoor_x, camcoor_y2 if cameraID == 2 else camcoor_y3, camcoor_z)
+            settings.add_sensor(cameraDepth)
+
+            camcoor_y3 = camcoor_y + cambaseline
+
+        for episode, startPoint in enumerate(startPoints):
+            settings.set(WeatherId = random.choice(weathers))
             # Start a new episode.
-
-            # Create a CarlaSettings object. This object is a wrapper around
-            # the CarlaSettings.ini file. Here we set the configuration we
-            # want for the new episode.
-            settings = CarlaSettings()
-            settings.set(
-                SynchronousMode=True,
-                SendNonPlayerAgentsInfo=True,
-                NumberOfVehicles=40,
-                NumberOfPedestrians=0,
-                WeatherId=random.choice([1, 2, 8, 9]),
-                QualityLevel=args.quality_level)
-            settings.randomize_seeds()
-
-            # Now we want to add a couple of cameras to the player vehicle.
-            # We will collect the images produced by these cameras every
-            # frame.
-
-            for cameraID in range(2, 4):
-                # The default camera captures RGB images of the scene.
-                cameraRGB = Camera('Camera%dRGB' % cameraID, FOV=camFOV)
-                # Set image resolution in pixels.
-                cameraRGB.set_image_size(resolution_w, resolution_h)
-                # Set its position relative to the car in meters.
-                cameraRGB.set_position(camcoor_x, camcoor_y, camcoor_z)
-                settings.add_sensor(cameraRGB)
-
-                # Let's add another camera producing ground-truth depth.
-                cameraDepth = Camera('Camera%dDepth' % cameraID, PostProcessing='Depth', FOV=camFOV)
-                cameraDepth.set_image_size(resolution_w, resolution_h)
-                cameraDepth.set_position(camcoor_x, camcoor_y, camcoor_z)
-                settings.add_sensor(cameraDepth)
-
-                camcoor_y = camcoor_y + cambaseline
-
             scene = client.load_settings(settings)
-
-            # Choose one player start at random.
-            number_of_player_starts = len(scene.player_start_spots)
-            player_start = random.randint(0, max(0, number_of_player_starts - 1))
-
             print('Starting new episode at %r...' % scene.map_name)
-            client.start_episode(player_start)
+            client.start_episode(startPoint)
 
+            tic = time.time()
             # Iterate every frame in the episode.
-            for frame in range(0 - num_frames_skip, frames_per_episode):
+            for frame in range(0 - num_frames_skip, args.frames_per_episode):
 
                 # Read the data produced by the server this frame.
                 measurements, sensor_data = client.read_data()
@@ -96,27 +108,14 @@ def run_carla_client(args):
                         filename = args.out_filename_format.format(episode, name, frame)
                         measurement.save_to_disk(filename, lambda depth: camfu * cambaseline / depth, 'pfm')
 
-                # We can access the encoded data of a given image as numpy
-                # array using its "data" property. For instance, to get the
-                # depth value (normalized) at pixel X, Y
-                #
-                #     depth_array = sensor_data['CameraDepth'].data
-                #     value_at_pixel = depth_array[Y, X]
-                #
-
-                # Now we have to send the instructions to control the vehicle.
-                # If we are in synchronous mode the server will pause the
-                # simulation until we send this control.
-
-                # Together with the measurements, the server has sent the
-                # control that the in-game autopilot would do this frame. We
-                # can enable autopilot by sending back this control to the
-                # server. We can modify it if wanted, here for instance we
-                # will add some noise to the steer.
-
                 control = measurements.player_measurements.autopilot_control
                 control.steer += random.uniform(-0.02, 0.02)
                 client.send_control(control)
+                print('time left: %.2f' % ((time.time() - tic) / 3600 * (
+                        (args.frames_per_episode - frame)
+                        + (args.frames_per_episode + num_frames_skip) * (len(startPoints) - episode - 1)))
+                      )
+                tic = time.time()
 
 
 def print_measurements(measurements):
@@ -137,7 +136,7 @@ def print_measurements(measurements):
         other_lane=100 * player_measurements.intersection_otherlane,
         offroad=100 * player_measurements.intersection_offroad,
         agents_num=number_of_agents)
-    print_over_same_line(message)
+    print(message)
 
 
 def main():
@@ -174,6 +173,16 @@ def main():
         default=2,
         type=float,
         help='scale of frames')
+    argparser.add_argument(
+        '-n', '--num_starts',
+        default=0,
+        type=int,
+        help='number of start points (episode)')
+    argparser.add_argument(
+        '-f', '--frames_per_episode',
+        default=100,
+        type=int,
+        help='number of frames per episode')
 
     args = argparser.parse_args()
 
